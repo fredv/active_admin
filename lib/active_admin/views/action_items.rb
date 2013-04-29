e862d642c98168ae14061f730393202ecb1eedb6
Frederik Vollert <frederik.vollert@dynport.de>
2013-04-29 15:00:00 +0200

    updates action items

--- lib/active_admin/views/action_items.rb
+++ lib/active_admin/views/action_items.rb
@@ -5,9 +5,9 @@ module ActiveAdmin
 
       def build(action_items)
         div class: "btn-group pull-right" do
-          action_items.each do |action_item|
+          [action_items.map do |action_item|
             instance_eval(&action_item.block)
-          end
+          end].join("\n").html_safe
         end
       end
 
