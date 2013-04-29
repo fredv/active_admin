200c949c6741fc166bf58d2cbff59c5cf1c9bce8
Frederik Vollert <frederik.vollert@dynport.de>
2013-04-29 12:34:20 +0200

    updates cancel linkl

--- lib/active_admin/form_builder.rb
+++ lib/active_admin/form_builder.rb
@@ -21,7 +21,7 @@ module ActiveAdmin
     end
 
     def cancel_link(url = {:action => "index"}, html_options = {}, li_attrs = {})
-      li_attrs[:class] ||= "cancel"
+      li_attrs[:class] ||= "btn btn-mini cancel"
       li_content = template.link_to I18n.t('active_admin.cancel'), url, html_options
       form_buffers.last << li_content
     end
