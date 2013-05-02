3898d779ed182fd4180a13ab00d589e187979cbc
Frederik Vollert <frederik.vollert@dynport.de>
2013-05-02 11:23:50 +0200

    updates filter check boxes

--- lib/active_admin/inputs/filter_check_boxes_input.rb
+++ lib/active_admin/inputs/filter_check_boxes_input.rb
@@ -2,7 +2,7 @@ module ActiveAdmin
   module Inputs
     class FilterCheckBoxesInput < ::Formtastic::Inputs::CheckBoxesInput
       include FilterBase
-
+ 
       def input_name
         "#{object_name}[#{association_primary_key || method}_in][]"
       end
@@ -18,7 +18,7 @@ module ActiveAdmin
 
       # Don't wrap in UL tag
       def choices_group_wrapping(&block)
-        template.capture(&block)
+        template.content_tag :div, template.capture(&block), class: 'controls'
       end
 
       # Don't wrap in LI tag
