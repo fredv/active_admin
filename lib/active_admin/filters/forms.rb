6200f7f6b50962cf13df345b575817e27ae4e4ac
Frederik Vollert <frederik.vollert@dynport.de>
2013-05-30 00:29:53 +0200

    updates filter form

--- lib/active_admin/filters/forms.rb
+++ lib/active_admin/filters/forms.rb
@@ -68,7 +68,7 @@ module ActiveAdmin
         options[:url] ||= collection_path
         options[:html] ||= {}
         options[:html][:method] = :get
-        options[:html][:class] ||= "filter_form"
+        options[:html][:class] = "filter_form"
         options[:as] = :q
         clear_link = link_to(I18n.t('active_admin.clear_filters'), "#", :class => "btn btn-mini clear_filters_btn")
         form_for search, options do |f|
